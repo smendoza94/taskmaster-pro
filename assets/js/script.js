f06133b22cc4be2e2cb20bcf7378956b0bdd6fd2
StevenM <stevenbmendoza@gmail.com>
2022-03-05 13:02:23 -0800

    added clickable date editor per each task

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,6 +113,37 @@ $(".list-group").on("click","span", function() {
   dateInput.trigger("focus");
 });
 
+// value of due date was changed
+$(".list-group").on("blur","input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-","");
+
+  // get the task's position in the list of the li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan =  $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
