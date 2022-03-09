9cd29c3c3de718c8a9d53e723ad427cf14614e9b
StevenM <stevenbmendoza@gmail.com>
2022-03-08 23:36:15 -0800

    add due date handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -43,6 +45,21 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  // convert to moment object at 5:00 pm
+  var time = moment(date, "L").set("hour",17);
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time,"days")) <= 2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 $(".list-group").on("click","p", function() {
@@ -128,6 +145,11 @@ $("#trash").droppable({
   }
 });
 
+// JQuery date picker 
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
 $(".list-group").on("blur","textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
@@ -176,12 +198,21 @@ $(".list-group").on("click","span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // auto focus on new elements
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur","input[type='text']", function() {
+$(".list-group").on("change","input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -209,6 +240,9 @@ $(".list-group").on("blur","input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
