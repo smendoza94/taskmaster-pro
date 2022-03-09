3409835746c53480710006095e79365d61b287dd
StevenM <stevenbmendoza@gmail.com>
2022-03-08 17:08:23 -0800

    drag and drop, trash bin, mobile touch and drag

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,73 @@ $(".list-group").on("click","p", function() {
   console.log(textInput);
 });
 
+// jQuery sortable code for tasks and their containers
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate",this);
+  },
+  over: function(event) {
+    console.log("over",event.target);
+  },
+  out: function(event) {
+    console.log("out",event.target);
+  },
+  update: function(event) {
+    //array to store the task data in 
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-","");
+    
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr);
+  }
+});
+
+// jQuery code for dragable delete trash bin
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 $(".list-group").on("blur","textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
